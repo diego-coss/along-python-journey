8e05e67e7fe305cf135e1d55a34a3412d378fa00
Diego Coss <rdcos@icloud.com>
2024-07-10 17:55:53 -0700

    Hey, I removed let's and changed it to let us.

--- ex20.py
+++ ex20.py
@@ -13,7 +13,7 @@ print_all (current_file)
 print ("Now let's rewind, kind of like a tape.")
 
 rewind (current_file)
-print ("Let's print three lines:\n")
+print ("Let us print three lines:\n")
 current_line = 1
 print_a_line (current_line, current_file)
 
